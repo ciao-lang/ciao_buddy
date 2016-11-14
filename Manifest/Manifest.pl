2ac37a862b74a0e893271d67ee85e9014522ff3f
Jose F. Morales <jfmcjf@gmail.com>
2016-11-14 19:09:56 +0100

    Updated for latest version of Ciao

--- Manifest/Manifest.pl
+++ Manifest/Manifest.pl
@@ -1,10 +1,6 @@
-% Manifest file for Ciao-Buddy
-bundle_name(ciao_buddy).
-bundle_packname('Ciao-Buddy').
-bundle_version('0').
-bundle_patch('0').
-%
-bundle_requires([core]).
-bundle_alias_paths([
+:- bundle(ciao_buddy).
+version('0.1').
+depends([core]).
+alias_paths([
     ciao_buddy = 'src'
 ]).
